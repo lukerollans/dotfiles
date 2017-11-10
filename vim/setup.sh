cf2b9c79167d57226d772e37540f47cdf5390980
Luke Rollans <hello@lukerollans.me>
2017-11-10 13:57:45 +0800

    Add the_silver_searcher installation to vim setup script

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Configure vim, install plugins, and configure plugins if needed
 
+# Install The Silver Searcher for fast searching
+brew install the_silver_searcher
+
 # Install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
