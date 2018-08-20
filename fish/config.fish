4451cd7ea22631ec86968caf17316abcf78a4ae7
Luke Rollans <hello@lukerollans.me>
2018-08-20 21:23:00 +0800

    Add some fzf config to Fish

--- fish/config.fish
+++ fish/config.fish
@@ -3,3 +3,7 @@ set -U EDITOR vim
 
 # Setup asdf
 source ~/.asdf/asdf.fish
+
+# fzf for fuzzy finding
+# Use The Silver Searcher (`ag`) as the default fzf finder
+export FZF_DEFAULT_COMMAND='ag --nocolor -g ""'
