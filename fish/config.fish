dc6276759ee33dd75d04173d26d1d5fdbae7ef52
Luke Rollans <hello@lukerollans.me>
2018-08-22 15:28:44 +0800

    Add Google Cloud SDK to Fish

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,6 @@ source ~/.asdf/asdf.fish
 # fzf for fuzzy finding
 # Use The Silver Searcher (`ag`) as the default fzf finder
 export FZF_DEFAULT_COMMAND='ag --nocolor -g ""'
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/lukerollans/Downloads/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/lukerollans/Downloads/google-cloud-sdk/path.fish.inc'; else; . '/Users/lukerollans/Downloads/google-cloud-sdk/path.fish.inc'; end; end
