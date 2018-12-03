bf1272c96847d8cd34596d78904a321d21d22d42
Luke Rollans <hello@lukerollans.me>
2018-12-03 09:19:14 +0800

    Add gcloud to PATH in fish

--- fish/config.fish
+++ fish/config.fish
@@ -8,11 +8,11 @@ source ~/.asdf/asdf.fish
 # Use The Silver Searcher (`ag`) as the default fzf finder
 export FZF_DEFAULT_COMMAND='ag --nocolor -g ""'
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/lukerollans/Downloads/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/lukerollans/Downloads/google-cloud-sdk/path.fish.inc'; else; . '/Users/lukerollans/Downloads/google-cloud-sdk/path.fish.inc'; end; end
-
 # Add composer binaries to path
 set -gx PATH ~/.composer/vendor/bin $PATH
 
 # Add brew mysql binary to path
 set -gx PATH /usr/local/opt/mysql@5.7/bin $PATH
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/lukerollans/Desktop/google-cloud-sdk/path.fish.inc' ]; . '/Users/lukerollans/Desktop/google-cloud-sdk/path.fish.inc'; end
