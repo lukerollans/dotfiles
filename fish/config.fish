a71ad7830829815d475300d3f94424d44ae0d752
Luke Rollans <hello@lukerollans.me>
2019-05-13 13:42:00 +0800

    Remove some PHP stuff I hope to never use again!

--- fish/config.fish
+++ fish/config.fish
@@ -8,11 +8,5 @@ source ~/.asdf/asdf.fish
 # Use The Silver Searcher (`ag`) as the default fzf finder
 export FZF_DEFAULT_COMMAND='ag --nocolor -g ""'
 
-# Add composer binaries to path
-set -gx PATH ~/.composer/vendor/bin $PATH
-
-# Add brew mysql binary to path
-set -gx PATH /usr/local/opt/mysql@5.7/bin $PATH
-
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/lukerollans/Desktop/google-cloud-sdk/path.fish.inc' ]; . '/Users/lukerollans/Desktop/google-cloud-sdk/path.fish.inc'; end
