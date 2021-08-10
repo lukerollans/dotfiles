4e7ac1f3309ab53d18726b7e1da9ede39a10d7fc
Luke Rollans <luke@rollans.sh>
2021-08-10 12:06:43 +0800

    Read in Perx secrets

--- fish/config.fish
+++ fish/config.fish
@@ -30,3 +30,6 @@ set -g fish_user_paths "/usr/local/opt/openssl@1.1/bin" $fish_user_paths
 set -gx LDFLAGS "-L/usr/local/opt/openssl@1.1/lib"
 set -gx CPPFLAGS "-I/usr/local/opt/openssl@1.1/include"
 set -gx PKG_CONFIG_PATH "/usr/local/opt/openssl@1.1/lib/pkgconfig"
+
+# Read in Perx secrets
+source ~/.config/fish/perx.fish
