e12f2d8465b27074defe762eb5330e87711c5293
Luke Rollans <luke@rollans.sh>
2023-06-05 21:42:03 +0800

    vi and vim both open nvim

--- fish/config.fish
+++ fish/config.fish
@@ -8,6 +8,8 @@ export FZF_DEFAULT_COMMAND='ag --nocolor -g ""'
 # Set some arbitrary aliases I use
 alias mt "mix test"
 alias mc "mix compile"
+alias vim "nvim"
+alias vi "nvim"
 
 # Ensure Homebrew paths are available
 set -gx HOMEBREW_PREFIX "/opt/homebrew";
