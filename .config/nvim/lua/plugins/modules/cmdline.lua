ed613f5ffe2ada2991ce7115df84622013c61fd3
Luke Rollans <luke@rollans.sh>
2026-04-10 17:24:50 +0800

    lots of tweaks across the board

--- .config/nvim/lua/plugins/modules/cmdline.lua
+++ .config/nvim/lua/plugins/modules/cmdline.lua
@@ -2,8 +2,12 @@ return {
   "folke/noice.nvim",
   event = "VeryLazy",
   opts = {
-    command_palette = true,
     lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
+      },
       signature = {
         auto_open = {
           enabled = false,
