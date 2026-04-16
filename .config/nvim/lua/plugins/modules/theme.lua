247810703f3d9ed37db4599c6a2d2d8eace79bad
Luke Rollans <luke@rollans.sh>
2026-04-16 22:08:29 +0800

    install ember neovim theme

--- .config/nvim/lua/plugins/modules/theme.lua
+++ .config/nvim/lua/plugins/modules/theme.lua
@@ -1,5 +1,16 @@
 return {
   {
+    "ember-theme/nvim",
+    name = "ember",
+    priority = 1000,
+    config = function()
+      require("ember").setup({
+        variant = "ember", -- "ember" | "ember-soft" | "ember-light"
+      })
+      vim.cmd("colorscheme ember")
+    end
+  },
+  {
     "catppuccin/nvim",
     enabled = true,
     priority = 150,
