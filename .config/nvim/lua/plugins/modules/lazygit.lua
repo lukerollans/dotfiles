0c3bb19d1fd6b7199e25d149dac99b256d781067
Luke Rollans <luke@rollans.sh>
2026-08-26 13:23:15 +0800

    fix lazygit float transparency

--- .config/nvim/lua/plugins/modules/lazygit.lua
+++ .config/nvim/lua/plugins/modules/lazygit.lua
@@ -1,5 +1,5 @@
 -- integrates lazygit with neovim
-vim.g.lazygit_floating_window_winblend = 1 -- no transparency on floating window
+vim.g.lazygit_floating_window_winblend = 0 -- no transparency on floating window
 vim.g.lazygit_floating_window_scaling_factor = 0.8 -- make it a little smaller
 
 vim.pack.add({
